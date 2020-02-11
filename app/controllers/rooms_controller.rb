d3bf0793ff793846318968da7f238fd3994075f1
oginotsutomu <oginotsutomugigi@icloud.com>
2020-02-11 18:22:12 +0900

    room-index-create

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,5 @@
 class RoomsController < ApplicationController
+  def index
+    @rooms = [1,2,3,4,5,6,7,8,9,10]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+
+  resources :rooms, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
+  root 'rooms#index'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
